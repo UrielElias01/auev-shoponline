d749e4447c21ef744d335ad582ab81002eb481b2
UrielElias01 <urielelias.01@gmail.com>
2024-06-07 17:40:30 -0600

    Formulario de registro y login funcionales

--- auev-shop/src/app/componentes/login/login.component.ts
+++ auev-shop/src/app/componentes/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../servicios/auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,8 +19,26 @@ export class LoginComponent {
     ]]
   })
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb: FormBuilder, private authService: AuthService, private messageService: MessageService, private router:Router, private mensaje: MessageService) { }
 
+
+  login() {
+    console.log('Login');
+    const { email, password } = this.loginForm.value;
+
+    this.authService.getUserByEmail(email as string).subscribe(
+      response => {
+        if (response.length > 0 && response[0].password === password) {
+          sessionStorage.setItem('email', email as string);
+          this.router.navigate(['/home']);
+        } else {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Email o Contraseña Incorrecta' });
+        }
+      },
+      error => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Email o Contraseña Incorrecta' });
+      }
+    );
   }
 
   get email(){
